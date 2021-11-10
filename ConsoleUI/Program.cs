a77e0f7906b8288f28b8d3a48487ba038af738b7
olumide azeez afolabi <midas44000@yahoo.com>
2021-11-10 11:22:40 -0600

    initial commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,22 +32,41 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicle = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car volvo = new Car() { HasTrunk = true, Make = "Volvo", Model = "x45", year = 2018 };
+            Motorcycle motorcycle = new Motorcycle() { HasSideCart = true, Make = "BKY", Model = "Bike", year = 2004 };
+
+            Vehicle crv = new Car() { Make = "Honda", Model = "X4", year = 2017 };
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Lamborgini", year = 2011, Model = "V6" };
+
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicle.Add(volvo);
+            vehicle.Add(motorcycle);
+            vehicle.Add(crv);
+            vehicle.Add(sport);
 
-            // Call each of the drive methods for one car and one motorcycle
+            foreach (var veh in vehicle)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.year}");
+                veh.DriveAbstract();
+                veh.DriveVirtual();
 
-            #endregion            
-            Console.ReadLine();
+                // Call each of the drive methods for one car and one motorcycle
+
+                #endregion
+                Console.ReadLine();
+            }
+
         }
     }
 }
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int year { get; set; } = 1;
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+
+
+
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+        }
+    }
+}
+
